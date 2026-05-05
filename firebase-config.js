f073e9ab29a4750ff6519cb03215244b9e60110f
tianzilin0312-star <tianzilin0312@gmail.com>
2026-05-05 19:10:08 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,3 @@
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC9YmfiEp2ovI74IESkCNDx78V1FnZ9wpU",
   authDomain: "grocery-helper-43bfe.firebaseapp.com",
@@ -13,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "46805801530",
   appId: "1:46805801530:web:e068303d12040a241b3303"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
